4555f8f367dce83396ce9f5949126b094779ee60
Marten Prieß <marten@non-rocket-science.com>
2015-03-25 09:29:15 +0100

    Implemented ConnectorResource helper

--- vaadin-jcrop-sample/src/main/java/org/vaadin/jcrop/JCropSampleUI.java
+++ vaadin-jcrop-sample/src/main/java/org/vaadin/jcrop/JCropSampleUI.java
@@ -1,136 +1,154 @@
 package org.vaadin.jcrop;
 
+import javax.servlet.annotation.WebServlet;
+
+import org.vaadin.jcrop.selection.JcropSelection;
+import org.vaadin.jcrop.selection.JcropSelectionChanged;
+
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.data.util.converter.Converter;
 import com.vaadin.data.util.converter.StringToFloatConverter;
 import com.vaadin.data.util.converter.StringToIntegerConverter;
+import com.vaadin.server.StreamResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.*;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
-import org.vaadin.jcrop.selection.JcropSelection;
-import org.vaadin.jcrop.selection.JcropSelectionChanged;
-
-import javax.servlet.annotation.WebServlet;
-
 
 @Theme("valo")
 public class JCropSampleUI extends UI {
 
-    private Jcrop jcrop = new Jcrop();
-    private Label listenerLabel = new Label("cropEvent: ");
+	private final Jcrop jcrop = new Jcrop();
+	private final Label listenerLabel = new Label("cropEvent: ");
 
-    private String[] imageUrls = new String[]{"./VAADIN/images/pic-1.jpg", "./VAADIN/images/pic-2.jpg", "./VAADIN/images/pic-3.jpg"};
-    private int currentIndex = 1;
+	private final String[] imageUrls = new String[] { "./VAADIN/images/pic-1.jpg", "./VAADIN/images/pic-2.jpg", "./VAADIN/images/pic-3.jpg" };
+	private int currentIndex = 1;
 
-    private Button toggleEnabled;
-    private TextField minX = genNumberField("x", "0", new StringToIntegerConverter()), minY = genNumberField("y", "0", new StringToIntegerConverter());
-    private TextField maxX = genNumberField("x", "0", new StringToIntegerConverter()), maxY = genNumberField("y", "0", new StringToIntegerConverter());
-    private TextField aspectRatio = genNumberField("aspectRatio", "0", new StringToFloatConverter());
+	private Button toggleEnabled;
+	private final TextField minX = genNumberField("x", "0", new StringToIntegerConverter()), minY = genNumberField("y", "0", new StringToIntegerConverter());
+	private final TextField maxX = genNumberField("x", "0", new StringToIntegerConverter()), maxY = genNumberField("y", "0", new StringToIntegerConverter());
+	private final TextField aspectRatio = genNumberField("aspectRatio", "0", new StringToFloatConverter());
 
-    private TextField genNumberField(String caption, String initValue, Converter converter) {
-        TextField textField = new TextField();
-        textField.setValue(initValue);
-        textField.setConverter(converter);
-        textField.setInputPrompt(caption);
-        textField.setImmediate(true);
-        textField.setInvalidAllowed(false);
-        textField.setValidationVisible(true);
-        textField.setWidth(60, Unit.PIXELS);
-        textField.addStyleName(ValoTheme.TEXTFIELD_TINY);
-        return textField;
-    }
+	private TextField genNumberField(final String caption, final String initValue, final Converter converter) {
+		TextField textField = new TextField();
+		textField.setValue(initValue);
+		textField.setConverter(converter);
+		textField.setInputPrompt(caption);
+		textField.setImmediate(true);
+		textField.setInvalidAllowed(false);
+		textField.setValidationVisible(true);
+		textField.setWidth(60, Unit.PIXELS);
+		textField.addStyleName(ValoTheme.TEXTFIELD_TINY);
+		return textField;
+	}
 
+	@Override
+	protected void init(final VaadinRequest vaadinRequest) {
+		final VerticalLayout layout = new VerticalLayout();
+		layout.setMargin(true);
+		layout.setSpacing(true);
+		layout.setSizeFull();
 
-    @Override
-    protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setMargin(true);
-        layout.setSpacing(true);
-        layout.setSizeFull();
+		setContent(layout);
 
-        setContent(layout);
+		HorizontalLayout buttonLayout = new HorizontalLayout();
+		buttonLayout.setSpacing(true);
 
-        HorizontalLayout buttonLayout = new HorizontalLayout();
-        buttonLayout.setSpacing(true);
+		buttonLayout.addComponent(new Button("setSelectio", new Button.ClickListener() {
+			@Override
+			public void buttonClick(final ClickEvent clickEvent) {
+				JCropSampleUI.this.jcrop.setSelection(10, 10, 100, 50);
+			}
+		}));
+		buttonLayout.addComponent(new Button("clearSelection", new Button.ClickListener() {
+			@Override
+			public void buttonClick(final ClickEvent clickEvent) {
+				JCropSampleUI.this.jcrop.clearSelection();
+			}
+		}));
+		buttonLayout.addComponent(new Button("switchImage", new Button.ClickListener() {
+			@Override
+			public void buttonClick(final ClickEvent clickEvent) {
+				JCropSampleUI.this.jcrop.setImageUrl(JCropSampleUI.this.imageUrls[(++JCropSampleUI.this.currentIndex) % 3]);
+			}
+		}));
 
-        buttonLayout.addComponent(new Button("setSelectio", new Button.ClickListener() {
-            @Override
-            public void buttonClick(ClickEvent clickEvent) {
-                jcrop.setSelection(10, 10, 100, 50);
-            }
-        }));
-        buttonLayout.addComponent(new Button("clearSelection", new Button.ClickListener() {
-            @Override
-            public void buttonClick(ClickEvent clickEvent) {
-                jcrop.clearSelection();
-            }
-        }));
-        buttonLayout.addComponent(new Button("switchImage", new Button.ClickListener() {
-            @Override
-            public void buttonClick(ClickEvent clickEvent) {
-                jcrop.setImageUrl(imageUrls[(++currentIndex) % 3]);
-            }
-        }));
-        toggleEnabled = new Button("toggle isEnabled", new Button.ClickListener() {
-            @Override
-            public void buttonClick(ClickEvent clickEvent) {
-                if (toggleEnabled.getCaption().contains("isEnabled")) {
-                    jcrop.setEnabled(false);
-                    toggleEnabled.setCaption("toggle isDisabled");
-                } else {
-                    jcrop.setEnabled(true);
-                    toggleEnabled.setCaption("toggle isEnabled");
-                }
-            }
-        });
-        buttonLayout.addComponent(toggleEnabled);
-        layout.addComponent(buttonLayout);
+		buttonLayout.addComponent(new Button("setResource", new Button.ClickListener() {
+			@Override
+			public void buttonClick(final ClickEvent clickEvent) {
+				JCropSampleUI.this.jcrop.setResource(new StreamResource(new SampleStreamResource(), "sample-image.png"));
+			}
+		}));
+		this.toggleEnabled = new Button("toggle isEnabled", new Button.ClickListener() {
+			@Override
+			public void buttonClick(final ClickEvent clickEvent) {
+				if (JCropSampleUI.this.toggleEnabled.getCaption()
+						.contains("isEnabled")) {
+					JCropSampleUI.this.jcrop.setEnabled(false);
+					JCropSampleUI.this.toggleEnabled.setCaption("toggle isDisabled");
+				} else {
+					JCropSampleUI.this.jcrop.setEnabled(true);
+					JCropSampleUI.this.toggleEnabled.setCaption("toggle isEnabled");
+				}
+			}
+		});
+		buttonLayout.addComponent(this.toggleEnabled);
+		layout.addComponent(buttonLayout);
 
-        HorizontalLayout maxMinSizeLayout = new HorizontalLayout();
-        maxMinSizeLayout.setSpacing(true);
+		HorizontalLayout maxMinSizeLayout = new HorizontalLayout();
+		maxMinSizeLayout.setSpacing(true);
 
-        maxMinSizeLayout.addComponent(new Label("minSize"));
-        maxMinSizeLayout.addComponent(minX);
-        maxMinSizeLayout.addComponent(minY);
-        maxMinSizeLayout.addComponent(new Label("maxSize"));
-        maxMinSizeLayout.addComponent(maxX);
-        maxMinSizeLayout.addComponent(maxY);
-        maxMinSizeLayout.addComponent(new Label("aspectRatio"));
-        maxMinSizeLayout.addComponent(aspectRatio);
-        Button setMinMaxSelectionBtn = new Button("changeSettings", new Button.ClickListener() {
-            @Override
-            public void buttonClick(ClickEvent clickEvent) {
-                jcrop.setMinCropSize((int) minX.getConvertedValue(), (int) minY.getConvertedValue());
-                jcrop.setMaxCropSize((int) maxX.getConvertedValue(), (int) maxY.getConvertedValue());
-                jcrop.setAspectRatio((float) aspectRatio.getConvertedValue());
+		maxMinSizeLayout.addComponent(new Label("minSize"));
+		maxMinSizeLayout.addComponent(this.minX);
+		maxMinSizeLayout.addComponent(this.minY);
+		maxMinSizeLayout.addComponent(new Label("maxSize"));
+		maxMinSizeLayout.addComponent(this.maxX);
+		maxMinSizeLayout.addComponent(this.maxY);
+		maxMinSizeLayout.addComponent(new Label("aspectRatio"));
+		maxMinSizeLayout.addComponent(this.aspectRatio);
+		Button setMinMaxSelectionBtn = new Button("changeSettings", new Button.ClickListener() {
+			@Override
+			public void buttonClick(final ClickEvent clickEvent) {
+				JCropSampleUI.this.jcrop.setMinCropSize((int) JCropSampleUI.this.minX.getConvertedValue(), (int) JCropSampleUI.this.minY.getConvertedValue());
+				JCropSampleUI.this.jcrop.setMaxCropSize((int) JCropSampleUI.this.maxX.getConvertedValue(), (int) JCropSampleUI.this.maxY.getConvertedValue());
+				JCropSampleUI.this.jcrop.setAspectRatio((float) JCropSampleUI.this.aspectRatio.getConvertedValue());
 
-            }
-        });
-        setMinMaxSelectionBtn.addStyleName(ValoTheme.BUTTON_TINY);
-        maxMinSizeLayout.addComponent(setMinMaxSelectionBtn);
-        layout.addComponent(maxMinSizeLayout);
+			}
+		});
+		setMinMaxSelectionBtn.addStyleName(ValoTheme.BUTTON_TINY);
+		maxMinSizeLayout.addComponent(setMinMaxSelectionBtn);
+		layout.addComponent(maxMinSizeLayout);
 
+		layout.addComponent(this.listenerLabel);
 
-        layout.addComponent(listenerLabel);
+		this.jcrop.setWidth(750, Unit.PIXELS);
+		this.jcrop.setHeight(500, Unit.PIXELS);
+		this.jcrop.setImageUrl(this.imageUrls[this.currentIndex]);
+		layout.addComponent(this.jcrop);
+		layout.setExpandRatio(this.jcrop, 1);
+		this.jcrop.addListener(new JcropSelectionChanged() {
+			@Override
+			public void selectionChanged(final JcropSelection selection) {
+				JCropSampleUI.this.listenerLabel.setValue(selection.toString());
+			}
+		});
 
-        jcrop.setWidth(750, Unit.PIXELS);
-        jcrop.setHeight(500, Unit.PIXELS);
-        jcrop.setImageUrl(imageUrls[currentIndex]);
-        layout.addComponent(jcrop);
-        layout.setExpandRatio(jcrop, 1);
-        jcrop.addListener(new JcropSelectionChanged() {
-            @Override
-            public void selectionChanged(JcropSelection selection) {
-                listenerLabel.setValue(selection.toString());
-            }
-        });
-    }
+	}
 
-    @WebServlet(urlPatterns = "/*", name = "JCropSampleUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = org.vaadin.jcrop.JCropSampleUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
-    }
+	@WebServlet(
+			urlPatterns = "/*",
+			name = "JCropSampleUIServlet",
+			asyncSupported = true)
+	@VaadinServletConfiguration(
+			ui = org.vaadin.jcrop.JCropSampleUI.class,
+			productionMode = false)
+	public static class MyUIServlet extends VaadinServlet {
+	}
 }
--- vaadin-jcrop/src/main/java/org/vaadin/jcrop/Jcrop.java
+++ vaadin-jcrop/src/main/java/org/vaadin/jcrop/Jcrop.java
@@ -1,16 +1,26 @@
 package org.vaadin.jcrop;
 
-import com.vaadin.annotations.JavaScript;
-import com.vaadin.annotations.StyleSheet;
-import com.vaadin.ui.AbstractJavaScriptComponent;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
 import org.vaadin.jcrop.client.JcropServerRpc;
 import org.vaadin.jcrop.client.JcropState;
 import org.vaadin.jcrop.selection.JcropSelection;
 import org.vaadin.jcrop.selection.JcropSelectionChanged;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import com.vaadin.annotations.JavaScript;
+import com.vaadin.annotations.StyleSheet;
+import com.vaadin.server.ConnectorResource;
+import com.vaadin.server.Page;
+import com.vaadin.server.RequestHandler;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinResponse;
+import com.vaadin.server.VaadinSession;
+import com.vaadin.ui.AbstractJavaScriptComponent;
 
 /**
  * Vaadin wrapper for the jQuery Plugin Jcrop(https://github.com/tapmodo/Jcrop)<br>
@@ -19,179 +29,237 @@ import java.util.List;
  * @version 1.0
  */
 @StyleSheet("vaadin://jcrop/jquery.Jcrop.min.css")
-@JavaScript({"vaadin://jcrop/jquery-1.11.2.min.js", "vaadin://jcrop/jquery.Jcrop.min.js", "Jcrop.js"})
+@JavaScript({ "vaadin://jcrop/jquery-1.11.2.min.js", "vaadin://jcrop/jquery.Jcrop.min.js", "Jcrop.js" })
 public class Jcrop extends AbstractJavaScriptComponent {
 
+	private final List<JcropSelectionChanged> listeners = new ArrayList<>();
+	private JcropSelection selection = null;
 
-    private List<JcropSelectionChanged> listeners = new ArrayList<>();
-    private JcropSelection selection = null;
+	private ConnectorResource resource;
+	private String requestHandlerUri = null;
 
-    public Jcrop() {
-        /**
-         * register the ServerRpc to the server-implementation in order to listen to it's calls
-         */
-        registerRpc(new JcropServerRpc() {
-            @Override
-            public void cropSelectionChanged(Integer x, Integer y, Integer width, Integer height) {
-                if (Jcrop.this.isEnabled()) {
-                    // update the state values
-                    setSelection(x, y, width, height, false);
-                    // trigger listeners
-                    for (JcropSelectionChanged listener : listeners) {
-                        // selection will get updated within setSelection
-                        listener.selectionChanged(selection);
-                    }
-                }
-            }
-        });
-        setPrimaryStyleName("image-cropper");
-    }
+	public Jcrop() {
+		/**
+		 * register the ServerRpc to the server-implementation in order to listen to it's calls
+		 */
+		registerRpc(new JcropServerRpc() {
+			@Override
+			public void cropSelectionChanged(final Integer x, final Integer y, final Integer width, final Integer height) {
+				if (Jcrop.this.isEnabled()) {
+					// update the state values
+					setSelection(x, y, width, height, false);
+					// trigger listeners
+					for (JcropSelectionChanged listener : Jcrop.this.listeners) {
+						// selection will get updated within setSelection
+						listener.selectionChanged(Jcrop.this.selection);
+					}
+				}
+			}
+		});
+		setPrimaryStyleName("image-cropper");
+	}
 
-    /**
-     * will release the selection
-     */
-    public void clearSelection() {
-        getState().x = 0;
-        getState().y = 0;
-        getState().cropWidth = 0;
-        getState().cropHeight = 0;
-        callFunction("clearSelection");
-    }
+	/**
+	 * will release the selection
+	 */
+	public void clearSelection() {
+		getState().x = 0;
+		getState().y = 0;
+		getState().cropWidth = 0;
+		getState().cropHeight = 0;
+		callFunction("clearSelection");
+	}
 
-    /**
-     * change the base url of image. this will reinitialize the Jcrop.
-     *
-     * @param imageUrl
-     */
-    public void setImageUrl(String imageUrl) {
-        getState().imageUrl = imageUrl;
-        triggerRepaint();
-    }
+	/**
+	 * change the base url of image. this will reinitialize the Jcrop.
+	 *
+	 * @param imageUrl
+	 */
+	public void setImageUrl(final String imageUrl) {
+		getState().imageUrl = imageUrl;
+		triggerRepaint();
+	}
 
-    /**
-     * get the current selection state
-     *
-     * @return could be null
-     */
-    public JcropSelection getSelection() {
-        return selection;
-    }
+	/**
+	 * will initialize a RequestHandler and set the imageUrl to the handler
+	 * 
+	 * @param resource
+	 */
+	public void setResource(final ConnectorResource resource) {
+		initRequestHandler();
+		this.resource = resource;
+		setImageUrl(getRequestUri());
+	}
 
-    /**
-     * set the selection of Jcrop programmatically<br>
-     * is only working when the component is already attached to the screen
-     *
-     * @param x
-     * @param y
-     * @param width
-     * @param height
-     */
-    public void setSelection(int x, int y, int width, int height) {
-        setSelection(x, y, width, height, true);
-    }
+	/**
+	 * initalize a unique RequestHandler
+	 */
+	private void initRequestHandler() {
+		if (this.requestHandlerUri == null) {
+			this.requestHandlerUri = UUID.randomUUID()
+					.toString();
+			VaadinSession.getCurrent()
+					.addRequestHandler(new RequestHandler() {
 
-    /**
-     * set the selection of Jcrop programmatically<br>
-     * is only working when the component is already attached to the screen
-     *
-     * @param selection
-     */
-    public void setSelection(JcropSelection selection) {
-        setSelection(selection.getX(), selection.getY(), selection.getWidth(), selection.getHeight(), true);
-    }
+						@Override
+						public boolean handleRequest(final VaadinSession session, final VaadinRequest request, final VaadinResponse response)
+								throws IOException {
+							if (String.format("/%s", Jcrop.this.requestHandlerUri)
+									.equals(request.getPathInfo())) {
+								Jcrop.this.resource.getStream()
+										.writeResponse(request, response);
+								return true;
+							} else {
+								return false;
+							}
+						}
+					});
+		}
+	}
 
-    private void setSelection(int x, int y, int width, int height, boolean triggerSelection) {
-        getState().x = x;
-        getState().y = y;
-        getState().cropWidth = width;
-        getState().cropHeight = height;
-        selection = new JcropSelection(x, y, width, height, width <= 0 && height <= 0);
-        if (triggerSelection) {
-            callFunction("setSelection", x, y, width, height);
-        }
-    }
+	/**
+	 * generate's the requestUri for the RequestHandler with a ts-parameter to avoid caching of browser on resource-changes
+	 * 
+	 * @return
+	 */
+	private String getRequestUri() {
+		URI uri = Page.getCurrent()
+				.getLocation();
+		String url = null;
+		if ((uri.getPort() == 80) || (uri.getPort() == 443) || (uri.getPort() <= 0)) {
+			url = uri.getScheme() + "://" + uri.getHost() + uri.getPath();
+		} else {
+			url = uri.getScheme() + "://" + uri.getHost() + ":" + uri.getPort() + uri.getPath();
+		}
+		if (url != null && !url.endsWith("/")) {
+			url += "/";
+		}
+		url += this.requestHandlerUri + "?ts=" + new Date().getTime();
+		return url;
+	}
 
-    /**
-     * set the aspectRatio of the selection<br>
-     * Aspect ratio of w/h (e.g. 1 for square)<br>
-     * by default it's 0 and so disabled
-     *
-     * @param aspectRatio
-     */
-    public void setAspectRatio(float aspectRatio) {
-        if (getState().aspectRatio != aspectRatio) {
-            getState().aspectRatio = aspectRatio;
-            triggerRepaint();
-        }
-    }
+	/**
+	 * get the current selection state
+	 *
+	 * @return could be null
+	 */
+	public JcropSelection getSelection() {
+		return this.selection;
+	}
 
-    /**
-     * set the minimum size of the crop selection<br>
-     * Minimum width/height, use 0 for unbounded dimension<br>
-     * by default it's unbounded
-     *
-     * @param x
-     * @param y
-     */
-    public void setMinCropSize(int x, int y) {
-        if (getState().minSizeX != x || getState().minSizeY != y) {
-            getState().minSizeX = x;
-            getState().minSizeY = y;
-            triggerRepaint();
-        }
-    }
+	/**
+	 * set the selection of Jcrop programmatically<br>
+	 * is only working when the component is already attached to the screen
+	 *
+	 * @param x
+	 * @param y
+	 * @param width
+	 * @param height
+	 */
+	public void setSelection(final int x, final int y, final int width, final int height) {
+		setSelection(x, y, width, height, true);
+	}
 
-    /**
-     * set the maximum size of the crop selection<br>
-     * Minimum width/height, use 0 for unbounded dimension<br>
-     * by default it's unbounded
-     *
-     * @param x
-     * @param y
-     */
-    public void setMaxCropSize(int x, int y) {
-        if (getState().maxSizeX != x || getState().maxSizeY != y) {
-            getState().maxSizeX = x;
-            getState().maxSizeY = y;
-            triggerRepaint();
-        }
-    }
+	/**
+	 * set the selection of Jcrop programmatically<br>
+	 * is only working when the component is already attached to the screen
+	 *
+	 * @param selection
+	 */
+	public void setSelection(final JcropSelection selection) {
+		setSelection(selection.getX(), selection.getY(), selection.getWidth(), selection.getHeight(), true);
+	}
 
+	private void setSelection(final int x, final int y, final int width, final int height, final boolean triggerSelection) {
+		getState().x = x;
+		getState().y = y;
+		getState().cropWidth = width;
+		getState().cropHeight = height;
+		this.selection = new JcropSelection(x, y, width, height, width <= 0 && height <= 0);
+		if (triggerSelection) {
+			callFunction("setSelection", x, y, width, height);
+		}
+	}
 
-    /**
-     * add a selection on the selectionChanged event
-     *
-     * @param listener
-     */
-    public void addListener(JcropSelectionChanged listener) {
-        this.listeners.add(listener);
-    }
+	/**
+	 * set the aspectRatio of the selection<br>
+	 * Aspect ratio of w/h (e.g. 1 for square)<br>
+	 * by default it's 0 and so disabled
+	 *
+	 * @param aspectRatio
+	 */
+	public void setAspectRatio(final float aspectRatio) {
+		if (getState().aspectRatio != aspectRatio) {
+			getState().aspectRatio = aspectRatio;
+			triggerRepaint();
+		}
+	}
 
-    /**
-     * remove a selection of the selectionChanged event
-     *
-     * @param listener
-     */
-    public void removeListener(JcropSelectionChanged listener) {
-        this.listeners.remove(listener);
-    }
+	/**
+	 * set the minimum size of the crop selection<br>
+	 * Minimum width/height, use 0 for unbounded dimension<br>
+	 * by default it's unbounded
+	 *
+	 * @param x
+	 * @param y
+	 */
+	public void setMinCropSize(final int x, final int y) {
+		if (getState().minSizeX != x || getState().minSizeY != y) {
+			getState().minSizeX = x;
+			getState().minSizeY = y;
+			triggerRepaint();
+		}
+	}
 
-    /**
-     * explicitly map the custom state object to the server implementation
-     *
-     * @return
-     */
-    @Override
-    protected JcropState getState() {
-        return (JcropState) super.getState();
-    }
+	/**
+	 * set the maximum size of the crop selection<br>
+	 * Minimum width/height, use 0 for unbounded dimension<br>
+	 * by default it's unbounded
+	 *
+	 * @param x
+	 * @param y
+	 */
+	public void setMaxCropSize(final int x, final int y) {
+		if (getState().maxSizeX != x || getState().maxSizeY != y) {
+			getState().maxSizeX = x;
+			getState().maxSizeY = y;
+			triggerRepaint();
+		}
+	}
 
-    /**
-     * used only to reinitialize the jCrop component when needed
-     */
-    private void triggerRepaint() {
-        this.getState().triggerRepaint = String.valueOf(new Date().getTime());
-    }
+	/**
+	 * add a selection on the selectionChanged event
+	 *
+	 * @param listener
+	 */
+	public void addListener(final JcropSelectionChanged listener) {
+		this.listeners.add(listener);
+	}
 
+	/**
+	 * remove a selection of the selectionChanged event
+	 *
+	 * @param listener
+	 */
+	public void removeListener(final JcropSelectionChanged listener) {
+		this.listeners.remove(listener);
+	}
+
+	/**
+	 * explicitly map the custom state object to the server implementation
+	 *
+	 * @return
+	 */
+	@Override
+	protected JcropState getState() {
+		return (JcropState) super.getState();
+	}
+
+	/**
+	 * used only to reinitialize the jCrop component when needed
+	 */
+	private void triggerRepaint() {
+		this.getState().triggerRepaint = String.valueOf(new Date().getTime());
+	}
 
 }
